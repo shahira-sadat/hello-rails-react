c4b87244f11fe4b68556355404fd9866b37301ea
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-23 03:58:47 +0430

    Create model and add 5 greetings to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(text: 'Assalaam Alaikum')
+Greeting.create(text: 'hello')
+Greeting.create(text: 'hi')
+Greeting.create(text: 'hey')
+Greeting.create(text: 'hola')
